1f61ffacc55509690a73a5a3e2cb6a24f5ea2b5d
Matthew-Graham <mggraham94@gmail.com>
2019-06-10 23:02:34 +0100

    Added more fields to display in nodelist and test list

--- fnapp/platforms/android/app/src/main/assets/www/views/NodeListView.js
+++ fnapp/platforms/android/app/src/main/assets/www/views/NodeListView.js
@@ -8,16 +8,6 @@ function NodeListView(nodeList) {
 
 NodeListView.prototype.compile = function () {
 
-  Handlebars.registerHelper('convertFromUnixTime', function (unixTime) {
-    
-    //https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
-    var timestamp = unixTime; // replace your timestamp
-    var date = new Date(timestamp * 1000);
-    var formattedDate = ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear() + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
-    return new Handlebars.SafeString(formattedDate);
-  });
-
-
   Handlebars.registerHelper('statusColour', function (status) {
     if (status == "Passed") {
       return new Handlebars.SafeString("badge badge-positive");
