5492f89b262c1e58fe59de5d82eba94dea37cf77
Matthew-Graham <mggraham94@gmail.com>
2019-06-10 22:19:30 +0100

    Remove unix time conversion code
    Redundant json reponse has required field

--- fnapp/platforms/android/app/src/main/assets/www/views/TestListView.js
+++ fnapp/platforms/android/app/src/main/assets/www/views/TestListView.js
@@ -4,6 +4,28 @@ function TestListView(testListData){
 }
 
 TestListView.prototype.compile = function(){
+
+    Handlebars.registerHelper('testStatusColour', function (status) {
+        if (status == "Passed") {
+          return new Handlebars.SafeString("badge badge-positive");
+        } else if (status == "Failed") {
+          return new Handlebars.SafeString("badge badge-negative");
+        } else if (status == "Warning") {
+          return new Handlebars.SafeString("badge badge-primary");
+        } else if (status== "Untested"){
+            return new Handlebars.SafeString("badge badge-outlined");
+        }    
+        else{
+          return new Handlebars.SafeString("status colour error");
+        }
+      });
+    
+
+
+
+
+
+
     let testListTemplate = Handlebars.compile($("#testListTemplate").html());
     let testListHtml = testListTemplate(this.testListData);
     $(".content-padded").html(testListHtml);
