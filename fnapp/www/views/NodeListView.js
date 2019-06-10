96c7e20c33160d866fac601c4e25d1e11958fb00
Matthew-Graham <mggraham94@gmail.com>
2019-06-10 21:51:21 +0100

    Added handlbars handler for alert colour, added function for converting unix to normal date format,added date of last run test field for a node to display
    The added handler checks the alerttext field and modifies css class accordingly.
    Code for converting unix to dd/mm/yyyy hh/mm added for display

--- fnapp/www/views/NodeListView.js
+++ fnapp/www/views/NodeListView.js
@@ -1,12 +1,35 @@
 function NodeListView(nodeList) {
   router.currPage = "nodes";
-  console.log("NODE PAGE---"+router.currPage)
+  console.log("NODE PAGE---" + router.currPage)
   this.nodeList = nodeList;
   this.compile();
   this.attachEvents();
 }
 
 NodeListView.prototype.compile = function () {
+
+  Handlebars.registerHelper('convertFromUnixTime', function (unixTime) {
+    
+    //https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+    var timestamp = unixTime; // replace your timestamp
+    var date = new Date(timestamp * 1000);
+    var formattedDate = ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear() + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+    return new Handlebars.SafeString(formattedDate);
+  });
+
+
+  Handlebars.registerHelper('statusColour', function (status) {
+    if (status == "Passed") {
+      return new Handlebars.SafeString("badge badge-positive");
+    } else if (status == "Failed") {
+      return new Handlebars.SafeString("badge badge-negative");
+    } else if (status == "Warning") {
+      return new Handlebars.SafeString("badge badge-primary");
+    } else {
+      return new Handlebars.SafeString("status colour error");
+    }
+  });
+
   let nodeListTemplate = Handlebars.compile($("#nodesTemplate").html());
   let nodeHtml = nodeListTemplate(this.nodeList);
   $(".content-padded").html(nodeHtml);
@@ -14,22 +37,22 @@ NodeListView.prototype.compile = function () {
   /**
    * Nav bar 
    */
-   //let nav =  new NavbarView();
+  //let nav =  new NavbarView();
 
-   /**Header */
-   let headerTemplate = Handlebars.compile($("#headerTemplate").html());
-   let context = {title:sessionStorage.getItem("serverName")};
-   let headerHTML = headerTemplate(context);
-   $("#topHeader").html(headerHTML);
-   
+  /**Header */
+  let headerTemplate = Handlebars.compile($("#headerTemplate").html());
+  let context = { title: sessionStorage.getItem("serverName") };
+  let headerHTML = headerTemplate(context);
+  $("#topHeader").html(headerHTML);
+
 }
 
 NodeListView.prototype.attachEvents = function () {
   $(".table-view-cell").on('click', function (event) {
     let nodeid = this.id;
     let pageid = "tests";
-   // pRouter = new PageRouter();   
-    router.routeToPage(pageid,nodeid);
+    // pRouter = new PageRouter();   
+    router.routeToPage(pageid, nodeid);
   });
 }
 
